3e19591648a5bbfc0d084079e994f2d35cae9e19
Michael Welch <michael.g.welch@jci.com>
2021-02-11 13:06:14 -0600

    Add FullWebResponse switch to debug script

--- Invoke-MetasysMethod.ps1
+++ Invoke-MetasysMethod.ps1
@@ -8,8 +8,9 @@ param(
     [string]$Method = "Get",
     [Int]$Version = 4,
     [switch]$SkipCertificateCheck,
-    [string]$Reference
+    [string]$Reference,
+    [switch]$FullWebResponse
 )
 Import-Module -Force -Name ./Invoke-MetasysMethod
 # mget-object "WIN-21DJ9JV9QH6:EECMI-NCE25-2/MV1" -SkipCertificateCheck:$SkipCertificateCheck
-Invoke-MetasysMethod -SiteHost $SiteHost -UserName $UserName -Path $Path -Body $Body -Method $Method -Version $Version -Reference $Reference -SkipCertificateCheck:$SkipCertificateCheck -Clear:$Clear -Login:$Login
+Invoke-MetasysMethod -SiteHost $SiteHost -UserName $UserName -Path $Path -Body $Body -Method $Method -Version $Version -Reference $Reference -SkipCertificateCheck:$SkipCertificateCheck -Clear:$Clear -Login:$Login -FullWebResponse:$FullWebResponse
