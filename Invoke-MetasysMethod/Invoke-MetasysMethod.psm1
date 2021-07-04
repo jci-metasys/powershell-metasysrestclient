2cfc58979d7b2ddae7c5ddaaa86fed84c8c2d2e6
Michael Welch <michael.g.welch@jci.com>
2021-07-04 15:20:54 -0500

    feat: extract buildUri into own file

--- Invoke-MetasysMethod/Invoke-MetasysMethod.psm1
+++ Invoke-MetasysMethod/Invoke-MetasysMethod.psm1
@@ -88,26 +88,6 @@ function Invoke-MetasysMethod {
 
     Set-StrictMode -Version 2
 
-    # The path can be
-    # * relative to the https://hostname/api/v{next}
-    # * absolute (eg https://hostname/api/v{next}/objects/{id}/attributes/presentValue)
-    function buildUri {
-        param (
-            [string]$siteHost = [MetasysEnvVars]::getSiteHost(),
-            [int]$version = [MetasysEnvVars]::getVersion(),
-            [string]$baseUri = "api",
-            [string]$path
-        )
-
-        $uri = [Uri]::new($path, [UriKind]::RelativeOrAbsolute)
-        if ($uri.IsAbsoluteUri) {
-            return $uri
-        }
-
-        $fullPath = "https://$siteHost/$([Path]::Join($baseUri, "v" + $version, $path))"
-        return [Uri]::new($fullPath)
-    }
-
     # The uri can be
     # * relative to the https://hostname/api/v{next}
     # * absolute (eg https://hostname/api/v{next}/objects/{id}/attributes/presentValue)
