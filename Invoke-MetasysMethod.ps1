2c7b2ec8f34ad524e4412b0ab6da538a709c8484
Michael Welch <michael.g.welch@jci.com>
2021-07-04 15:04:31 -0500

    feat: extract password mgmt funcs to new file
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be kept; you may remove them yourself if you want to.
    # An empty message aborts the commit.
    #
    # Date:      Sun Jul 4 15:04:31 2021 -0500
    #
    # interactive rebase in progress; onto 43d776d
    # Last commands done (3 commands done):
    #    e e5ca9b0 feat: extract MetasysEnvVars into separate scipt
    #    e ffdb260 feat: extract password mgmt funcs to new file
    # Next commands to do (7 remaining commands):
    #    e 01e246d feat: encapsualte all access to env vars
    #    e c95103b feat: extract buildUri into own file
    # You are currently splitting a commit while rebasing branch 'rebase-branch' on '43d776d'.
    #
    # Changes to be committed:
    #	modified:   Invoke-MetasysMethod.ps1
    #	modified:   Invoke-MetasysMethod/Invoke-MetasysMethod.psd1
    #	modified:   Invoke-MetasysMethod/Invoke-MetasysMethod.psm1
    #	new file:   Invoke-MetasysMethod/password-management.ps1
    #

--- Invoke-MetasysMethod.ps1
+++ Invoke-MetasysMethod.ps1
@@ -9,8 +9,9 @@ param(
     [string]$Method = "Get",
     [Int]$Version,
     [switch]$SkipCertificateCheck,
-    [hashtable]$Headers
+    [hashtable]$Headers,
+    [string]$DeleteCredentials
 )
 Import-Module -Force -Name ./Invoke-MetasysMethod
 # mget-object "WIN-21DJ9JV9QH6:EECMI-NCE25-2/MV1" -SkipCertificateCheck:$SkipCertificateCheck
-Invoke-MetasysMethod -SiteHost $SiteHost -UserName $UserName -Path $Path -Body $Body -Method $Method -Version $Version -SkipCertificateCheck:$SkipCertificateCheck -Clear:$Clear -Login:$Login -Header $Headers
+Invoke-MetasysMethod -SiteHost $SiteHost -UserName $UserName -Path $Path -Body $Body -Method $Method -Version $Version -SkipCertificateCheck:$SkipCertificateCheck -Clear:$Clear -Login:$Login -Header $Headers -DeleteCredentials $DeleteCredentials
--- Invoke-MetasysMethod/Invoke-MetasysMethod.psm1
+++ Invoke-MetasysMethod/Invoke-MetasysMethod.psm1
@@ -140,67 +140,6 @@ function Invoke-MetasysMethod {
         return $request
     }
 
-    function find-internet-user {
-        param (
-            [string]$siteHost
-        )
-
-        if ($IsMacOS) {
-            $cred = Invoke-Expression "security find-internet-password -s $siteHost 2>/dev/null"
-            if ($cred) {
-                $userNameLine = $cred | Where-Object { $_.StartsWith("    ""acct") }
-                if ($userNameLine) {
-                    $userName = $userNameLine.Split('=')[1].Trim('"')
-                    return $userName
-                }
-            }
-        }
-    }
-
-    function find-internet-password {
-        param (
-            [string]$siteHost,
-            [string]$userName
-        )
-
-        if (!($IsMacOS)) {
-            return
-        }
-
-        $passwordEntry = Invoke-Expression "security find-internet-password -s $siteHost -a $userName -w 2>/dev/null"
-        if ($passwordEntry) {
-            return ConvertTo-SecureString $passwordEntry -AsPlainText
-        }
-    }
-
-    function clear-internet-password {
-        param(
-            [String]$siteHost
-        )
-
-        if (!$IsMacOS) {
-            return
-        }
-
-        Invoke-Expression "security delete-internet-password -s $siteHost 1>/dev/null"
-    }
-    function add-internet-password {
-        param(
-            [string]$siteHost,
-            [string]$userName,
-            [SecureString]$password
-        )
-
-        if (!($IsMacOS)) {
-            return
-        }
-
-        $plainText = ConvertFrom-SecureString -SecureString $password -AsPlainText
-
-        Invoke-Expression "security add-internet-password -U -s $siteHost -a $userName -w $plainText -c mgw1  "
-
-    }
-
     if ($PSVersionTable.PSEdition -ne "Core") {
         Write-Error "Windows Powershell is not supported. Please install PowerShell Core"
         Write-Error "See https://github.com/powershell/powershell"
