0c1460a85ef842eb8d6d13265c6d319d8c7c20de
Michael Welch <michael.g.welch@jci.com>
2021-08-14 11:01:51 -0500

    refact: Extract MockConsole for tests

--- test/Connect-MetasysAccount.Tests.ps1
+++ test/Connect-MetasysAccount.Tests.ps1
@@ -16,63 +16,11 @@ Tests for ConnectMetasys
 Combinations
 
 #>
-class MockConsole {
-    static [string] $MetasysHostPrompt = "Metasys Host"
-    static [string] $UserNamePrompt = "UserName"
-    static [string] $PasswordPrompt = "Password"
-
-
-    static [String] $DefaultMetasysHost = "testhost"
-    static [String] $DefaultUserName = "testuser"
-    static [SecureString] $DefaultPassword = (ConvertTo-SecureString "testpassword" -AsPlainText)
-
-    [Hashtable]$Inputs = @{ }
-
-    MockConsole() {
-        $this.Inputs[[MockConsole]::MetasysHostPrompt] = [MockConsole]::DefaultMetasysHost
-        $this.Inputs[[MockConsole]::UserNamePrompt] = [MockConsole]::DefaultUserName
-        $this.Inputs[[MockConsole]::PasswordPrompt] = [MockConsole]::DefaultPassword
-    }
-
-    MockConsole([String]$SiteHost = $DefaultSiteHost, [string]$UserName = $DefaultUserName,
-        [SecureString]$Password = $DefaultPassword) {
-
-        $this.Inputs[[MockConsole]::MetasysHostPrompt] = $SiteHost
-        $this.Inputs[[MockConsole]::UserNamePrompt] = $UserName
-        $this.Inputs[[MockConsole]::PasswordPrompt] = $Password
-    }
-
-    [void] SetResponse([string]$Prompt, [object]$Response) {
-        $this.Inputs[$Prompt] = $Response
-    }
-
-    [Object] ReadHost([String]$Prompt) {
-        return $this.Inputs[$Prompt]
-    }
-
-    [String]GetUserName() {
-        return $this.Inputs[[MockConsole]::UserNamePrompt]
-    }
-
-    [String]GetPasswordAsPlainText() {
-        return $this.Inputs[[MockConsole]::PasswordPrompt] | ConvertFrom-SecureString -AsPlainText
-    }
-
-    [SecureString]GetPassword() {
-        return $this.Inputs[[MockConsole]::PasswordPrompt]
-    }
-
-    [String]GetMetasysHost() {
-        return $this.Inputs[[MockConsole]::MetasysHostPrompt]
-    }
-}
-
-
-
-
 
 BeforeAll {
     $mod = Import-Module -Name ../src/MetasysRestClient -Force -PassThru
+
+    . ./MockConsole.ps1
 
     Set-Variable -Name LatestVersion -Value 4 -Option Constant
 
